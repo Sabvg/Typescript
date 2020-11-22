250737cd7255113ecbf0738fd8b4c146bc9590ec
Sabvg <Sabela.vazquez.gago@gmail.com>
2020-11-22 18:26:41 +0100

    Update controller.ts

--- controllers/controller.ts
+++ controllers/controller.ts
@@ -24,8 +24,9 @@ function createCar(){
     }else if(!validatePlate(plate)) {
         acumError++;
         alert("The plate must have 4 numbers followed by 3 letters")
-        
-    } else {
+    } 
+    
+    if(acumError == 0){
         car = new Car(plate, color, brand);
 
         const carInfo = document.getElementById('carInfo');
@@ -50,12 +51,6 @@ function createCar(){
         document.getElementById('carForm')?.classList.add('no-display');
         document.getElementById('createCar')?.classList.add('no-display');
     } 
-
-    if (acumError > 0) {
-        return false;
-    } else {
-        return true;
-    }
 }
 
 //WHEELS:
@@ -64,43 +59,33 @@ let wheel: Wheel[] = [];
    
 function createWheel(){
     let acumErrorWheel = 0;
-
+    
     for (let i = 1; i <= 4; i++) {
 
         let wheelDiameter: number = parseFloat((<HTMLInputElement>document.getElementById('wheelDiameter'+ i)).value);
-        
         let wheelBrand: string = (<HTMLInputElement>document.getElementById('wheelBrand' + i)).value.toString();
-        
-          if(!validateDiameter(wheelDiameter)) {
+            if(!validateDiameter(wheelDiameter)) {
                 acumErrorWheel++;
                 wheel.splice(0, wheel.length);
                 return alert('The diameter of wheel' + i + ' has to measure between 0.4 and 2');
                 //con return termina, no sigue leyendo el code y desaparece el error en consola de que no encuentra diameter. Los alert salen ahora uno a uno pero si voy corrigiendo los diámetros sólo pinta el 1 y 2 repetidos, con el splice se arregla
-                
-
             } else if(wheelBrand == "") {
                 wheel.splice(0, wheel.length);
                 alert("Brand " + i + " field must be filled.");
                 acumErrorWheel++;
             } 
-            
-            if(acumErrorWheel == 0) {
-                wheel.push(new Wheel(wheelDiameter, wheelBrand));
-            }
     }
 
-    for(let i=0; i<= 4; i++) {
-        car.addWheel(wheel[i]);
+    if(acumErrorWheel == 0) {
+        for(let i=1; i<= 4; i++) {
+            let wheelDiameter: number = parseFloat((<HTMLInputElement>document.getElementById('wheelDiameter'+ i)).value);  
+            let wheelBrand: string = (<HTMLInputElement>document.getElementById('wheelBrand' + i)).value.toString();
+            
+            let wheel = new Wheel(wheelDiameter, wheelBrand);
+            car.addWheel(wheel);
+        }
     }
-        
     printWheel();
-        
-    if (acumErrorWheel > 0) {
-        return false;
-    } else {
-        return true;
-    }
-    
 //resetForm();
 //quería probar a hacer que se limpiara el formulario al crearse una rueda y se limpia pero si escribo nuevos datos y le doy al botón "Create wheels" vuelve a pintar los datos anteriores.
 }
@@ -113,10 +98,10 @@ function printWheel() {
     <div class="card mt-4 p-3 style="width: 10rem;">
         <div class="card-body>
             <h3 class="card-title">Your wheels created:</h3>
-            <p class="card-text mt-3"><b>Wheel Diameter 1</b>: ${wheel[0].diameter} --- <b>Wheel Brand 1</b>: ${wheel[0].brand}</p>
-            <p class="card-text"><b>Wheel Diameter 2</b>: ${wheel[1].diameter} --- <b>Wheel Brand 2</b>: ${wheel[1].brand}</p>
-            <p class="card-text"><b>Wheel Diameter 3</b>: ${wheel[2].diameter} --- <b>Wheel Brand 3</b>: ${wheel[2].brand}</p>
-            <p class="card-text"><b>Wheel Diameter 4</b>: ${wheel[3].diameter} --- <b>Wheel Brand 4</b>: ${wheel[3].brand}</p>
+            <p class="card-text mt-3"><b>Wheel Diameter 1</b>: ${car.wheels[0].diameter} --- <b>Wheel Brand 1</b>: ${car.wheels[0].brand}</p>
+            <p class="card-text"><b>Wheel Diameter 2</b>: ${car.wheels[1].diameter} --- <b>Wheel Brand 2</b>: ${car.wheels[1].brand}</p>
+            <p class="card-text"><b>Wheel Diameter 3</b>: ${car.wheels[2].diameter} --- <b>Wheel Brand 3</b>: ${car.wheels[2].brand}</p>
+            <p class="card-text"><b>Wheel Diameter 4</b>: ${car.wheels[3].diameter} --- <b>Wheel Brand 4</b>: ${car.wheels[3].brand}</p>
         </div>
     </div>`;
     carWheel?.appendChild(element2);
